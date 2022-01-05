ec9e021be06bba608412af38cc36eaa92c45e1a6
mihara0320 <mxxxxi.ixxxxa@gmail.com>
2022-01-05 22:21:19 +0200

    comment out auth logics

--- src/templates/PageContainer/index.tsx
+++ src/templates/PageContainer/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { useRouter } from 'next/router'
+// import { useRouter } from 'next/router'
 import { useToast } from '@chakra-ui/react'
-import useUnsavedConfirm from 'hooks/useUnsavedConfirm'
+// import useUnsavedConfirm from 'hooks/useUnsavedConfirm'
 import useRequest from 'hooks/useRequest'
-import useAuth from 'hooks/useAuth'
-import { publicRoutes } from 'constants/common'
+// import useAuth from 'hooks/useAuth'
+// import { publicRoutes } from 'constants/common'
 
 export type PageContainerProps = {
   children: React.ReactNode
@@ -13,17 +13,16 @@ export type PageContainerProps = {
 export const PageContainer: React.FC<PageContainerProps> = ({
   children,
 }: PageContainerProps) => {
-  const router = useRouter()
-  const { getLoginUser } = useAuth()
+  // const router = useRouter()
+  // const { getLoginUser } = useAuth()
   const { state: requestState } = useRequest()
-  const unsavedConfirm = useUnsavedConfirm()
+  // const unsavedConfirm = useUnsavedConfirm()
   const toast = useToast()
 
-  const [authChecked, setAuthChecked] = React.useState(false)
-
-  const isPublicRoute = React.useMemo(() => {
-    return publicRoutes.indexOf(router.asPath.split('?')[0]) >= 0
-  }, [router.asPath])
+  // const [authChecked, setAuthChecked] = React.useState(false)
+  // const isPublicRoute = React.useMemo(() => {
+  //   return publicRoutes.indexOf(router.asPath.split('?')[0]) >= 0
+  // }, [router.asPath])
 
   React.useEffect(() => {
     window.addEventListener('unhandledrejection', (event) => {
@@ -35,62 +34,62 @@ export const PageContainer: React.FC<PageContainerProps> = ({
     })
   }, [toast])
 
-  React.useEffect(() => {
-    const handleBeforeUnload = (event: BeforeUnloadEvent) => {
-      const message = '編集中のデータは破棄されますがよろしいですか？'
-      event.preventDefault()
-      event.returnValue = message
-      return message
-    }
+  // React.useEffect(() => {
+  //   const handleBeforeUnload = (event: BeforeUnloadEvent) => {
+  //     const message = '編集中のデータは破棄されますがよろしいですか？'
+  //     event.preventDefault()
+  //     event.returnValue = message
+  //     return message
+  //   }
+  //
+  //   if (unsavedConfirm.confirm) {
+  //     window.addEventListener('beforeunload', handleBeforeUnload)
+  //   }
+  //
+  //   return () => window.removeEventListener('beforeunload', handleBeforeUnload)
+  // }, [unsavedConfirm.confirm])
 
-    if (unsavedConfirm.confirm) {
-      window.addEventListener('beforeunload', handleBeforeUnload)
-    }
+  // React.useEffect(() => {
+  //   const beforeRouteHandler = () => {
+  //     if (
+  //       unsavedConfirm.confirm &&
+  //       !window.confirm('未保存のデータは破棄されます。よろしいですか?')
+  //     ) {
+  //       router.events.emit('routeChangeError')
+  //       // `Route change to "${url}" was aborted (this error can be safely ignored). See https://github.com/zeit/next.js/issues/2476.`
+  //       throw `routing-cancelled`
+  //     }
+  //   }
+  //
+  //   router.events.on('routeChangeStart', beforeRouteHandler)
+  //
+  //   return () => {
+  //     router.events.off('routeChangeStart', beforeRouteHandler)
+  //   }
+  // }, [unsavedConfirm.confirm, router])
 
-    return () => window.removeEventListener('beforeunload', handleBeforeUnload)
-  }, [unsavedConfirm.confirm])
+  // React.useEffect(() => {
+  //   checkAuth()
+  // }, []) // eslint-disable-line react-hooks/exhaustive-deps
 
-  React.useEffect(() => {
-    const beforeRouteHandler = () => {
-      if (
-        unsavedConfirm.confirm &&
-        !window.confirm('未保存のデータは破棄されます。よろしいですか?')
-      ) {
-        router.events.emit('routeChangeError')
-        // `Route change to "${url}" was aborted (this error can be safely ignored). See https://github.com/zeit/next.js/issues/2476.`
-        throw `routing-cancelled`
-      }
-    }
-
-    router.events.on('routeChangeStart', beforeRouteHandler)
-
-    return () => {
-      router.events.off('routeChangeStart', beforeRouteHandler)
-    }
-  }, [unsavedConfirm.confirm, router])
-
-  React.useEffect(() => {
-    checkAuth()
-  }, []) // eslint-disable-line react-hooks/exhaustive-deps
-
-  const checkAuth = async () => {
-    const user = await getLoginUser()
-    console.log(user)
-
-    if (isPublicRoute && user) {
-      // Reload page to safely remove data remaining on memory
-      location.href = '/'
-      return false
-    }
-
-    if (!isPublicRoute && !user) {
-      location.href = '/login'
-      return false
-    }
-
-    setAuthChecked(true)
-    return true
-  }
+  // const checkAuth = async () => {
+  //   const user = await getLoginUser()
+  //   console.log(user)
+  //
+  //   if (isPublicRoute && user) {
+  //     // Reload page to safely remove data remaining on memory
+  //     location.href = '/'
+  //     return false
+  //   }
+  //
+  //   if (!isPublicRoute && !user) {
+  //     location.href = '/login'
+  //     return false
+  //   }
+  //
+  //   setAuthChecked(true)
+  //   return true
+  // }
 
   const LoadingIndicator = React.useMemo(() => {
     if (requestState.loadings > 0) {
@@ -100,9 +99,9 @@ export const PageContainer: React.FC<PageContainerProps> = ({
     return null
   }, [requestState.loadings])
 
-  if (!authChecked) {
-    return <div>Auth checking...</div>
-  }
+  // if (!authChecked) {
+  //   return <div>Auth checking...</div>
+  // }
 
   return (
     <div className="page-container">
